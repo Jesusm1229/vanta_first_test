ae4b7964eccc25f3d641d6edfa5e8a898db02c74
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2022-02-02 10:20:55 -0400

    Initial commit. Adding Background

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<div id="run">
+  <Vanta></Vanta>
+</div>
+</template>
+
+<script>
+import Vanta from './components/Vanta/Vanta.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Vanta
+  }
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+
+    box-sizing: border-box;
+
+    font-family: Arial, Helvetica, sans-serif;
+  }
+   div#run {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
---
+++ src/components/Vanta/Vanta.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="Vanta" ref="vanta">
+        <v-vanta effect="globe" :options=options></v-vanta>
+  </div>
+</template>
+ 
+<script>
+  import VVanta from 'vue-vanta';
+  export default {
+    name: "Vanta",
+    components: { VVanta },
+    data () {
+      return {
+        options: {
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        scale: 1.0,        
+        scaleMobile: 1.0,
+        color: "#ffffff",
+        backgroundColor: "#1b83c3",
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+div#Vanta{
+    width: 100%;
+    min-height:100vh;
+
+    position:absolute;
+    z-index:-1;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    
+
+  
+}
+
+</style>
